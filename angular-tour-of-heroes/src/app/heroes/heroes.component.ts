a5371ba686d9916a00bd385d431e7cf3aaa731c0
Yu0614 <azu369yu@gmail.com>
2019-05-04 17:21:09 +0900

    feat : finish chapter.2 - Display lists

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
@@ -14,8 +16,15 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
   }
 
-  marvel_heroes : Hero = {
+  hero : Hero = {
     id: 1 ,
     name: 'Iron man'
   };
+
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
 }
